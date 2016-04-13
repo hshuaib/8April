b7c893822b658cbe5d219c7dcbd3a69d7c4d7235
Hanan <hshuaib@qatar.cmu.edu>
2016-04-13 03:18:20 +0300

    addedFAQinterface

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -16,6 +16,7 @@ class Applicant < ActiveRecord::Base
   has_attached_file :membership, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image,:cv, :membership,  content_type: /\Aimage\/.*\Z/
   
+  
 
   validates :name, :disability,  presence: true
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409175654) do
+ActiveRecord::Schema.define(version: 20160412213123) do
 
   create_table "applicants", force: :cascade do |t|
     t.string   "name"
@@ -107,6 +107,13 @@ ActiveRecord::Schema.define(version: 20160409175654) do
     t.text     "body"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "q"
+    t.text     "ans"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
